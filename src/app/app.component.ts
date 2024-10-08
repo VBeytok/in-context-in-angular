62fd9cfdefb3e74f5cc2c3e2eebf23f340c1ff68
Vitalii Smolinskyi <vs@crowdin.com>
2024-10-08 09:41:33 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,35 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppComponent {
   title = 'jipt-in-angular';
+  VAR_SELECT: string = 'PERSONAL';
+  profile = {
+    name: 'account',
+    fullyFormattedID: '1234567890',
+    balance: 1000
+  };
+
+  ngOnInit() {
+    var jiptConfig = document.createElement('script');
+    jiptConfig.type = 'text/javascript';
+    jiptConfig.text = `
+      var _jipt = [];
+      _jipt.push(['project', '7da6d433eda3c9aeb9f96faf75f71c90']);
+      _jipt.push(['domain', 'beytok']);
+    `;
+
+    document.head.appendChild(jiptConfig);
+
+    var jiptScript = document.createElement('script');
+    jiptScript.type = 'text/javascript';
+    jiptScript.src = '//cdn.crowdin.com/jipt/jipt.js';
+
+    document.head.appendChild(jiptScript);
+
+    console.log('App component initialized');
+  }
 }
